a0daca1b31242a57047490eae34407e233577b51
Dmnoiik <d.marszalkiewicz2000@gmail.com>
2021-10-28 18:17:34 +0200

    add little fixes and pushing through

--- script.js
+++ script.js
@@ -3,31 +3,32 @@
 function computerPlay() {
     let number = Math.floor(Math.random() * 3) + 1;
     if (number === 1) {
-        return "Rock";
+        return "rock";
     } else if (number === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     };
 }
 
 
 function playRound() {
     let playerSelection = prompt("Rock, paper or scissors?");
+    playerSelection = playerSelection.toLowerCase();
     let computerSelection = computerPlay();
-    if (playerSelection === "Rock" && computerSelection === "Scissors" 
-    || playerSelection === "Scissors" && computerSelection === "Paper" 
-    || playerSelection === "Paper" && computerSelection === "Rock") {
+    if (playerSelection === "rock" && computerSelection === "scissors" 
+    || playerSelection === "scissors" && computerSelection === "aper" 
+    || playerSelection === "paper" && computerSelection === "rock") {
          console.log(`Player chose ${playerSelection} and Computer chose ${computerSelection}. Results: Player wins! `)
-         return "Player"
-    } else if (playerSelection === "Rock" && computerSelection === "Rock" 
-    || playerSelection === "Scissors" && computerSelection === "Scissors" 
-    || playerSelection === "Paper" && computerSelection === "Paper") {
+         return "player"
+    } else if (playerSelection === "rock" && computerSelection === "rock" 
+    || playerSelection === "scissors" && computerSelection === "scissors" 
+    || playerSelection === "paper" && computerSelection === "paper") {
         console.log(`Player chose ${playerSelection} and Computer chose ${computerSelection}. Results: That's a draw :(`)
         return "draw"
-    } else if (playerSelection === "Rock" && computerSelection === "Paper" 
-    || playerSelection === "Scissors" && computerSelection === "Rock" 
-    || playerSelection === "Paper" && computerSelection === "Scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "paper" 
+    || playerSelection === "scissors" && computerSelection === "ock" 
+    || playerSelection === "paper" && computerSelection === "scissors") {
         console.log(`Player chose ${playerSelection} and Computer chose ${computerSelection}. Results: Oof, computer wins this one`)
         return "computer"
     } else {
@@ -35,12 +36,12 @@ function playRound() {
     }
 }
 
-function playGame() {
+function playGameFive() {
     for (let i = 0; i <= 5; i++) {
         console.log(playRound());
     }
 }
-console.log(playGame());
+console.log(playGameFive());
 
 
 
